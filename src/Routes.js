77455c7da0ce48112ec1b3aa550e771f6ee8ec39
Krisz Magyar <krisz.magyar@gmail.com>
2018-03-13 09:11:37 +0100

    added play and 404 route

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import { Lobby } from './Lobby';
-import { CreateGame } from './CreateGame'
-import { JoinGame } from './JoinGame'
-
+import { Lobby } from './components/Lobby';
+import { CreateGame } from './components/CreateGame'
+import { JoinGame } from './components/JoinGame'
+import { PlayGame } from './components/PlayGame'
 
 export const Routes = () => (
     <Switch>
         <Route path = '/' exact component = {Lobby} />
         <Route path = '/create' exact component = {CreateGame} />
         <Route path = '/join' exact component = {JoinGame} />
+        <Route path = '/play/:id' component = {PlayGame} />
+        <Route path = '/' render={() => 404} />
     </Switch>
 )
---
+++ src/components/PlayGame.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const PlayGame = () => (
+    <div className = 'PlayGame'> 
+        <div>Play game</div>
+    </div>
+)
